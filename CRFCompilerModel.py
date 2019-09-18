b97a19121b249004df1641867901e76163d958ca
Hooney (chulhun Hwang) <qewqsa@naver.com>
2019-09-18 16:37:36 +0900

    2019-09-18, ver0.0.3, add CRF Compiler Model class

---
+++ CRFCompilerModel.py
@@ -0,0 +1,96 @@
+import os
+import pandas as pd
+import sklearn_crfsuite
+from sklearn_crfsuite import scorers
+from sklearn_crfsuite import metrics
+
+
+class CRFCompilerModel:
+    def __init__(self):
+        print("Make Idiom Set")
+
+    def dictToXY(self, dict, type):
+        data_x = []
+        data_y = []
+        for key in dict.keys():
+            data_x.append({key: dict.get(key)[0]})
+            if(dict.get(key)[0] == type):
+                data_y.append("1")
+            else:
+                data_y.append("0")
+
+        return data_x, data_y
+
+    def kfold(self, list, k, index):
+        size = len(list)
+        s_size = int(size/k)
+        s_array= []
+
+        if(size % k > 0):
+            flag = True
+        else:
+            flag = False
+
+        for num in range(0, k, 1):
+            if(num == (k-1)):
+                if(flag):
+                    s_array.append(list[(num * s_size):])
+                else:
+                    s_array.append(list[(num * s_size): ((num + 1) * s_size)])
+            else:
+                s_array.append(list[(num * s_size): ((num + 1) * s_size)])
+
+        train = []
+        test = []
+        for num in range(0, len(s_array), 1):
+            if(index == (num)):
+                test = test + s_array[num]
+            else:
+                train = train+s_array[num]
+
+
+        return train, test
+
+    def train(self, train_x, train_y, epo=30):
+        self.crfModel = sklearn_crfsuite.CRF(
+            algorithm='lbfgs',
+            c1=0.1,
+            c2=0.1,
+            max_iterations=100,
+            all_possible_transitions=True
+        )
+
+        max_size = len(train_x)
+        split_size = int(max_size/epo)
+        if(max_size%epo > 0):
+            split_size=split_size+1
+
+        for index in range(0, split_size, 1):
+            self.crfModel.fit(train_x[(index*epo):((index+1)*epo)],
+                              train_y[(index*epo):((index+1)*epo)])
+
+    def getLabels(self):
+        labels = list(self.crfModel.classes_)
+        return labels
+
+    def test(self, test_x, test_y, labels, epo=30):
+        max_size = len(test_x)
+        split_size = int(max_size / epo)
+        if (max_size % epo > 0):
+            split_size = split_size + 1
+
+        pred_y=[]
+        for index in range(0, split_size, 1):
+             temp_pred = self.crfModel.predict(test_x[(index*epo):((index+1)*epo)])
+             pred_y= pred_y+temp_pred
+
+        print(metrics.flat_f1_score(test_y, pred_y,
+                      average='weighted', labels=labels))
+
+        sorted_labels = sorted(
+            labels,
+            key=lambda name: (name[1:], name[0])
+        )
+        print(metrics.flat_classification_report(
+            test_y, pred_y, labels=sorted_labels, digits=3
+        ))
--- main.py
+++ main.py
@@ -2,21 +2,104 @@ import pandas as pd
 import numpy as np
 
 from MakeIdiomSet import MakeIdiomSet
+from CRFCompilerModel import CRFCompilerModel
 
 type_array = ["gcc", "icc", "msvc", "xcode"]
 makeIdiom = MakeIdiomSet()
 
 gcc_idiom_dict = makeIdiom.directoryToIdiom("./data/gcj_text/ass/gcc/", type_array[0])
-makeIdiom.checkDict(gcc_idiom_dict)
-makeIdiom.saveIdiomDict(gcc_idiom_dict, type_array[0])
+# makeIdiom.checkDict(gcc_idiom_dict)
+# makeIdiom.saveIdiomDict(gcc_idiom_dict, type_array[0])
 icc_idiom_dict = makeIdiom.directoryToIdiom("./data/gcj_text/ass/icc/", type_array[1])
-makeIdiom.checkDict(icc_idiom_dict)
-makeIdiom.saveIdiomDict(icc_idiom_dict, type_array[1])
+# makeIdiom.checkDict(icc_idiom_dict)
+# makeIdiom.saveIdiomDict(icc_idiom_dict, type_array[1])
 msvc_idiom_dict = makeIdiom.directoryToIdiom("./data/gcj_text/ass/msvc/", type_array[2])
-makeIdiom.checkDict(msvc_idiom_dict)
-makeIdiom.saveIdiomDict(msvc_idiom_dict, type_array[2])
+# makeIdiom.checkDict(msvc_idiom_dict)
+# makeIdiom.saveIdiomDict(msvc_idiom_dict, type_array[2])
 xcode_idiom_dict = makeIdiom.directoryToIdiom("./data/gcj_text/ass/xcode/", type_array[3])
-makeIdiom.checkDict(xcode_idiom_dict)
-makeIdiom.saveIdiomDict(xcode_idiom_dict, type_array[3])
+# makeIdiom.checkDict(xcode_idiom_dict)
+# makeIdiom.saveIdiomDict(xcode_idiom_dict, type_array[3])
+
+
+crfModel = CRFCompilerModel()
+fold = 5
+target = type_array[3]
+for index in range(0, fold, 1):
+    print("Index : " + str(index+1))
+    gcc_train, gcc_test = crfModel.kfold(gcc_idiom_dict, fold, index)
+    icc_train, icc_test = crfModel.kfold(icc_idiom_dict, fold, index)
+    msvc_train, msvc_test = crfModel.kfold(msvc_idiom_dict, fold, index)
+    xcode_train, xcode_test = crfModel.kfold(xcode_idiom_dict, fold, index)
+
+    train_x=[]
+    train_y=[]
+    test_x=[]
+    test_y=[]
+
+    train_list = None
+    if(target == type_array[0]):
+        train_list = gcc_train
+    elif(target == type_array[1]):
+        train_list = icc_train
+    elif(target == type_array[2]):
+        train_list = msvc_train
+    elif(target == type_array[3]):
+        train_list = xcode_train
+
+    for item_dict in train_list:
+        temp_x, temp_y = crfModel.dictToXY(item_dict, target)
+        train_x = train_x + temp_x
+        train_y = train_y + temp_y
+
+    for item_dict in gcc_test:
+        temp_x, temp_y = crfModel.dictToXY(item_dict, target)
+        test_x = test_x + temp_x
+        if (target == type_array[0]):
+            test_y = test_y + temp_y
+        else:
+            for num in temp_y:
+                test_y.append("0")
+
+    for item_dict in icc_test:
+        temp_x, temp_y = crfModel.dictToXY(item_dict, target)
+        test_x = test_x + temp_x
+        if (target == type_array[1]):
+            test_y = test_y + temp_y
+        else:
+            for num in temp_y:
+                test_y.append("0")
+
+    for item_dict in msvc_test:
+        temp_x, temp_y = crfModel.dictToXY(item_dict, target)
+        test_x = test_x + temp_x
+        if (target == type_array[2]):
+            test_y = test_y + temp_y
+        else:
+            for num in temp_y:
+                test_y.append("0")
+
+    for item_dict in xcode_test:
+        temp_x, temp_y = crfModel.dictToXY(item_dict, target)
+        test_x = test_x + temp_x
+        if (target == type_array[3]):
+            test_y = test_y + temp_y
+        else:
+            for num in temp_y:
+                test_y.append("0")
+
+    print("====> " + target)
+    print("> Train")
+    crfModel.train(train_x=train_x, train_y=train_y)
+
+    labels = crfModel.getLabels()
+
+    print(">Test")
+    crfModel.test(test_x=test_x, test_y=test_y, labels=labels)
+
+
+
+
+
+
 
 
